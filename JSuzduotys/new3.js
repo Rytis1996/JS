d46b3ec8f76e135b071f3a41af95cab9f671f596
Deividas <deiviuxs97@gmail.com>
2022-03-21 10:24:08 +0200

    pirmadienis

--- JSuzduotys/new3.js
+++ JSuzduotys/new3.js
@@ -26,11 +26,74 @@ komanda.forEach((item, index) => {
     divas.innerHTML = komanda[index].vardas + ", " + komanda[index].pavarde + ", " + komanda[index].greitis + ", " + komanda[index].ugis + ", " + komanda[index].amzius + ", " + komanda[index].ypatybes;
     document.body.appendChild(divas);
   });
+
+  komanda.sort(function(a, b){return a.greitis - b.greitis});
+
+  komanda.forEach((item, index) => {
+    var divas = document.createElement("div");
+    divas.innerHTML = komanda[index].greitis;
+    document.body.appendChild(divas);
+  });
+
   //  UZDUOTIS NR2
 
-  function masina(pavadinimas, greitis, kelias) {
+  // Masinu objekto konstruktorius
+  function masina(pavadinimas, laikas, kelias, keliasPerValanda) {
     this.pavadinimas = pavadinimas;
-    this.greitis = greitis;
+    this.laikas = laikas;
     this.kelias = kelias;
+    this.keliasPerValanda =   function keliasPerValanda() {
+      var keliasPerValanda = this.kelias / this.laikas;
+      return keliasPerValanda;
+    };
   };
+
+// kelias per valanda
+  // function keliasPerValanda(laikas, kelias) {
+  //   var keliasPerValanda = kelias / laikas;
+  //   return keliasPerValanda;
+  // };
+
+  // mygtuko sukurimas
+  var mygtukas = document.createElement("button");
+  document.body.appendChild(mygtukas);
+  mygtukas.setAttribute("id", "masinos5")
+  masinos5.innerHTML = 'masinos5';
+  masinos5.style.width = '100px';
+  masinos5.style.height = '80px';
+  masinos5.style.margin = '50px 100px';
+  masinos5.style.textAlign = 'center';
+  masinos5.style.margin = '0 auto';
+  masinos5.style.display = 'block';
+
+
+// Ivarikius pavadinimai ir parametrai
+var masinuPav = ['opel', 'audi', 'bmw', 'mazda', 'reno', 'volvo', 'toyota', 'subaru', 'ford', 'fiat', 'tesla'];
+var pavadinimas = masinuPav[Math.floor(Math.random() * masinuPav.length)];
+var laikas = Math.floor(Math.random() * (2990 - 30) + 30);
+var kelias = Math.floor(Math.random() * (200000 - 120000) + 120000);
+
+// masinu masyvas
+var masinos = [masina0, masina1, masina2, masina3, masina4];
+var masina0 = new masina('pavadinimas', 'laikas', 'kelias');
+var masina1 = new masina('pavadinimas', 'laikas', 'kelias');
+var masina2 = new masina('pavadinimas', 'laikas', 'kelias');
+var masina3 = new masina('pavadinimas', 'laikas', 'kelias');
+var masina4 = new masina('pavadinimas', 'laikas', 'kelias');
+
+console.log(masinos);
+
+  // mygtukas.addEventListener('click', function(){
+  //   var divas = document.createElement("div");
+  //   var newMasina = new masina(pavadinimas, laikas, kelias, keliasPerValanda) * 5;
+
+  //   masinos.push(newMasina);
+
+  //   masinos.forEach((item, index) => {
+
+  //   })
+  //   document.body.appendChild(divas);
+  // });
+
+
 
